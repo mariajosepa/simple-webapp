d83b567d2eae0296b06030f4f68b8c5a4e9b270e
TwitchViejoSarmi <alesansarive@gmail.com>
2024-11-18 03:15:36 -0500

    MOD: Agregadas nuevas APIs.

--- back/mysite/urls.py
+++ back/mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-    path('devices/', include('devices.urls')),
+    path('api/dispostivos/', include('dispositivos.urls')),
+    path('api/usuarios/', include('usuarios.urls')),
     path('admin/', admin.site.urls),
 ]
