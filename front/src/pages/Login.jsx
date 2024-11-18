875dcae7d06fc64fb42669906f4f74de937417d1
Jebuser <juanestebanbecerragutierrez@gmail.com>
2024-11-18 12:17:57 -0500

    MOD: Modificado el inicio de sesión para que coincidiera con el prototipo

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -2,16 +2,21 @@ import Button from "../components/Button";
 import Input from "../components/TextInput";
 import "../styles/Login.css";
 
-
 function Login() {
   return (
-    <div>
-      <h1>Iniciar Sesión</h1>
-      <p>Inicia sesión para continuar</p>
-      <Input label="Correo" />
-      <Input label="Contraseña" />
-      <Button label="Login" />
-      <p>¿Olvidó su contraseña?</p>
+    <div className="login-container">
+      <div className="login-box">
+        <h1>Iniciar Sesión</h1>
+        <p>Inicia sesión para continuar</p>
+        <form>
+          <Input label='CORREO' placeholder='quieropasarprocesos@example.com'/>
+          <Input label='CONTRASEÑA' placeholder='*******'/>
+          <button type="submit">Iniciar sesión</button>
+        </form>
+        <a href="/forgot-password" className="forgot-password">
+          ¿Olvidó su contraseña?
+        </a>
+      </div>
     </div>
   );
 }
